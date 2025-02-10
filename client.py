6320e914b8de092f99a0c2470824c1f74cd655e0
Suleyman <suley.kiani@gmail.com>
2025-02-10 17:15:43 -0500

    Modify client to send requests to Load Balancer

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import socket
 
-def send_request(port):
+def send_request():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(("127.0.0.1", port))
-    client_socket.send("Hello Server!".encode())
+    client_socket.connect(("127.0.0.1", 8000))  # Connect to Load Balancer
+    client_socket.send("Hello Load Balancer!".encode())
 
     response = client_socket.recv(1024).decode()
-    print(f"📩 Response from server {port}: {response}")
+    print(f"📩 Response from Load Balancer: {response}")
 
     client_socket.close()
 
 if __name__ == "__main__":
-    send_request(9001)
+    for _ in range(6):  # Send multiple requests
+        send_request()
+
